fc326a3b7531bed3ba39854ddf3db84cae28c006
xBrandonGx97 <45645923+xBrandonGx97@users.noreply.github.com>
2020-03-10 15:17:03 -0500

    Updates

---
+++ precache-manifest.7a824f8e5215a4f6d4208b91f4509923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08eb0d186ef6cba6aa65835922ae9bf6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ee4e61c61ef3ecafc914",
+    "url": "/monsters-rolodex/static/css/main.0f2423db.chunk.css"
+  },
+  {
+    "revision": "7fe0845a347327439809",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4e61c61ef3ecafc914",
+    "url": "/monsters-rolodex/static/js/main.0f399a24.chunk.js"
+  },
+  {
+    "revision": "0a7538a84e8ef0422b17",
+    "url": "/monsters-rolodex/static/js/runtime-main.984b980e.js"
+  }
+]);
